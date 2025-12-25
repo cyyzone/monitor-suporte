a55d82a36303ba0e4027313b3aabd90ec4f4705b
cyyzone <jenycds@hotmail.com>
2025-12-25 17:07:49 -0300

    Update dashboard_volume.py

--- dashboard_volume.py
+++ dashboard_volume.py
@@ -368,8 +368,8 @@ if btn_gerar:
                         "Nota": st.column_config.NumberColumn("Nota", format="%d ⭐")
                     }, use_container_width=True, hide_index=True
                 )
-        else:
-            st.warning("Nenhuma avaliação encontrada.")
+           else:
+                st.warning("Nenhuma avaliação encontrada.")
         else:
             st.warning("Nenhum dado bruto de CSAT retornado.")
 
